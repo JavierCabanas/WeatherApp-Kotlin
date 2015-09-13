cb704dc53a6ae861926f19892b9338f867fb184b
Juan Javier Cabanas <javiCabanas@videona.com>
2015-09-13 16:20:53 +0200

    chapter 11

--- app/src/main/java/com/javicabanas/weatherapp/domain/model/DomainClasses.kt
+++ app/src/main/java/com/javicabanas/weatherapp/domain/model/DomainClasses.kt
@@ -4,8 +4,11 @@ package com.javicabanas.weatherapp.domain.model
  * Created by Javi on 01/08/2015.
  */
 
-data class ForecastList(val city: String, val country:String,
-                    val dailyForecast: List<Forecast>)
+data class ForecastList(val city: String, val country: String,
+                        val dailyForecast: List<Forecast>) {
+    public fun get(position: Int): Forecast = dailyForecast[position]
+    public fun size(): Int = dailyForecast.size()
+}
 
 data class Forecast(val date: String, val description: String, val high: Int,
                     val low: Int)
